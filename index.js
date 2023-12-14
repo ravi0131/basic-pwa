37e6d022bb59af129e3b7644362509872dc0a62f
ravi0131 <ravi.kumar@stud.tu-darmstadt.de>
2023-12-14 07:50:11 +0100

    updated index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebas
 import { getDatabase, ref, push, onValue,remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
-    databaseURL: "https://playground-b49f9-default-rtdb.europe-west1.firebasedatabase.app/"
+    databaseURL: "" // TODO: add your db URL here
 }
 
 const app = initializeApp(appSettings)
